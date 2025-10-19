60f7910d990f2ef3cbd81aea0548397dc98a490a
Gaurav Rajput <joygrajput@gmail.com>
2025-10-19 23:51:04 +0530

    fixed deployment bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import UserDetails from "./components/UserDetails";
-import { UserProvider } from "./context/userContext";
+import { UserProvider } from "./context/UserContext";
 
 export default function App() {
   return (
